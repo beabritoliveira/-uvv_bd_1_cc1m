a48ca1f364a43ef0a1e2c6e25e80c5d304834536
beabritoliveira <103044603+beabritoliveira@users.noreply.github.com>
2022-05-06 23:44:31 -0300

    Update respostas.sql

--- PSet2/respostas.sql
+++ PSet2/respostas.sql
@@ -1,2 +1,30 @@
--- Pergunta 1.
+-- Pergunta 1
 select avg(salario) from funcionario;
+
+-- Pergunta 2
+
+-- Pergunta 3
+
+-- Pergunta 4
+
+-- Pergunta 5
+
+-- Pergunta 6
+
+-- Pergunta 7
+
+-- Pergunta 8
+
+-- Pergunta 9
+
+-- Pergunta 10
+
+-- Pergunta 11
+
+-- Pergunta 12
+
+-- Pergunta 13
+
+-- Pergunta 14
+
+-- Pergunta 15
