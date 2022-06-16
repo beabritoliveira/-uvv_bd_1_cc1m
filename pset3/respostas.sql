7bfc8b7efd7d887592db55b026258ceeef4f166c
beabritoliveira <103044603+beabritoliveira@users.noreply.github.com>
2022-06-16 16:47:05 -0300

    Update respostas.sql

--- pset3/respostas.sql
+++ pset3/respostas.sql
@@ -1,5 +1,5 @@
-SELECT codigo_pai, codigo, nome
-FROM classificacao
-INNER JOIN 
+SELECT c.codigo_pai, cc.nome, c.codigo, c.nome
+FROM classificacao c
+LEFT OUTER JOIN classificacao cc on (c.codigo_pai = cc.codigo) 
 ORDER BY codigo_pai asc, codigo asc
 ;
