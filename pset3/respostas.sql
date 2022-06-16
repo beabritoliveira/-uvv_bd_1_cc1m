613b34d1b507404a58115462a5461dd9cd77dc0d
beabritoliveira <103044603+beabritoliveira@users.noreply.github.com>
2022-06-16 18:25:12 -0300

    Update respostas.sql

--- pset3/respostas.sql
+++ pset3/respostas.sql
@@ -3,3 +3,16 @@ FROM classificacao c
 LEFT OUTER JOIN classificacao cc on (c.codigo_pai = cc.codigo) 
 ORDER BY codigo_pai asc, codigo asc
 ;
+
+
+WITH produtos AS (
+  SELECT  d.nome as descricao, cl.nome as classificacao, c.nome as especificacao
+  FROM classificacao c
+ INNER JOIN classificacao cl 
+	ON c.codigo_pai=cl.codigo
+ INNER JOIN classificacao d
+	ON cl.codigo_pai=d.codigo
+  WHERE d.codigo_pai IS NULL
+)
+SELECT * FROM produtos
+ORDER BY descricao ASC, classificacao ASC, especificacao ASC;
